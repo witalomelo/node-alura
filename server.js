369d3b89cc003c150f652a1a3dcb29583a7d4af9
witalomelo <witalovmelo@gmail.com>
2024-08-27 09:25:18 -0300

    criando o projeto com Node.js

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ import http from "http";
 
 const PORT = 3000;
 
+const rotas = {
+  "/": "curso de express api",
+  "/livros": "Entrei na rota livros",
+  "/autores": "Entrei na rota autores"
+};
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("curso node.js");
+  res.end(rotas[req.url]);
 });
 
 server.listen(PORT, () => {
